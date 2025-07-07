34a7eb17f0aa600b538b3e23460c634c62d5c9f2
Shamim Reza <rahaman.mosaic@gmail.com>
2025-07-08 03:17:14 +0600

    Add basic inventory resources

---
+++ app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'brand_id',
+        'category_id',
+        'unit_id',
+        'description',
+        'purchase_price',
+        'sales_price',
+        'alert_quantity',
+        'is_active',
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class);
+    }
+}
